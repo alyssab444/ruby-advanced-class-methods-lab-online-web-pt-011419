c8381f3f8b4169a9268e8bf17ab83fd731b1e858
Alyssa Brown <alyssa.brown@live.com>
2019-02-09 04:14:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,8 @@ class Song
   def self.alphabetical 
     @@all.sort_by {|song_name| song_name.name} 
   end 
-  def self.new_from_filename(file_name)
-    
+  def self.new_from_filename(filename)
+    info = filename.split("-")
   end 
   
 
