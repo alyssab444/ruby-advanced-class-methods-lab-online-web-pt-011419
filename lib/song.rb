e0244853f55ee49f39c19b7ffbf46b240193f0f1
Alyssa Brown <alyssa.brown@live.com>
2019-02-09 04:25:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
     info = filename.split("-")
     song= self.new 
     song.name= info[1].chomp(".mp3")
-    song.artist_name= info(0)
+    song.artist_name= info[0]
     song 
   end 
   
